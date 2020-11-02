33ec65298a14dd43030ef8def796cfdc7dc60e16
Kristofer Liljeblad <kristol@microsoft.com>
2020-11-02 17:57:39 +0100

    Sample HTTP/POST method implemented

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// To be removed
 type Post struct {
 	Title  string `json:"Rubrik"`
 	Author string `json:"Author"`
 	Text   string `json:"Text"`
 }
 
+// Whisper describes the messages being received or sent
+type Whisper struct {
+	Sender  string `json:"sender"`
+	Message string `json:"message"`
+}
+
+// To be removed
 func PostsHandler(w http.ResponseWriter, r *http.Request) {
 
 	posts := []Post{
@@ -23,10 +36,32 @@ func PostsHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(posts)
 }
 
+func newMessagePosted(w http.ResponseWriter, r *http.Request) {
+	log.Println("Message received")
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var whisper Whisper
+	json.Unmarshal(reqBody, &whisper)
+	log.Println("From:", whisper.Sender, "Message:", whisper.Message)
+
+	fmt.Fprintf(w, "Thanks for your message\n")
+	fmt.Fprintf(w, "%+v", string(reqBody))
+}
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/posts", PostsHandler)
+	router.HandleFunc("/", newMessagePosted).Methods("POST")
+
+	log.Println("Listening to requests on port 5051")
+	log.Fatal(http.ListenAndServe(":5051", router))
+}
+
 func main() {
-	http.HandleFunc("")
-	http.HandleFunc("/posts", PostsHandler)
-	http.ListenAndServe(":5051", nil)
+	log.Println("Whisper Service Started")
+
+	handleRequests()
 }
 
 /*
