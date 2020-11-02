790f7964c48ebe2e0215d93a3468aaa8dcd2ee74
Kristofer Liljeblad <kristol@microsoft.com>
2020-11-02 10:06:06 +0100

    First test version or REST Service

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Post struct {
+	Title  string `json:"Rubrik"`
+	Author string `json:"Author"`
+	Text   string `json:"Text"`
+}
+
+func PostsHandler(w http.ResponseWriter, r *http.Request) {
+
+	posts := []Post{
+		Post{"Post one", "Kristofer", "This is first post."},
+		Post{"Post two", "Jane", "This is second post."},
+		Post{"Post three", "John", "This is another post."},
+	}
+
+	json.NewEncoder(w).Encode(posts)
+}
+
+func main() {
+	http.HandleFunc("/posts", PostsHandler)
+	http.ListenAndServe(":5051", nil)
+}
